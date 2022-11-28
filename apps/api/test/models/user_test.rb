1baa77d26234985da182a95ab2e6a316b05cbf69
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-28 23:29:12 +0700

    (api) add user test

--- apps/api/test/models/user_test.rb
+++ apps/api/test/models/user_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save user when email follows valid form' do
+    assert User.new(email: 'test@example.com')
+  end
+
+  test 'should not save user when email is empty' do
+    assert_not User.new.save, 'User saved, even email is empty'
+  end
+
+  test "should not save user when email doesn't follow valid form" do
+    assert_not User.new(email: 'abc').save,
+               "User saved, even email doesn't follow valid form"
+  end
+
+  test 'should not save user when email already exists' do
+    email = 'test@example.com'
+    assert User.new(email: email).save, 'Failed to save user'
+    assert_not User.new(email: email).save,
+               'User saved, even email already exists'
+  end
 end
