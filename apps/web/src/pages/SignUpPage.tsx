b8024197eff3a4ffef5b464477c0edd2ed338343
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-29 19:51:16 +0700

    (web) add sign up form validation

--- apps/web/src/pages/SignUpPage.tsx
+++ apps/web/src/pages/SignUpPage.tsx
@@ -1,3 +1,36 @@
+import { Button, Container, Stack, TextField, Typography } from '@mui/material';
+import { Form, Formik } from 'formik';
+import SBTextField from '../components/SBTextField';
+import { UserCreation, UserCreationSchema } from '../schemas/UserCreation';
+
 export default function SignUpPage() {
-  return <h1>Sign up page</h1>;
+  return (
+    <Formik<UserCreation>
+      initialValues={{
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }}
+      validationSchema={UserCreationSchema}
+      onSubmit={console.log}
+    >
+      <Form>
+        <Container>
+          <Stack gap={2}>
+            <Typography variant="h1">Sign up</Typography>
+            <SBTextField label="Email" name="email" />
+            <SBTextField label="Password" type="password" name="password" />
+            <SBTextField
+              label="Confirm Password"
+              type="password"
+              name="confirmPassword"
+            />
+            <Button type="submit" variant="contained">
+              Sign up
+            </Button>
+          </Stack>
+        </Container>
+      </Form>
+    </Formik>
+  );
 }
