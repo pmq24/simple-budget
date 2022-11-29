33f3fdf8d4ba1015dd4337fad5d089c4a69095e3
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-11-29 20:13:20 +0700

    (web) add api folder

---
+++ apps/web/src/api/api.ts
@@ -0,0 +1,16 @@
+import axios, { Axios } from 'axios';
+import { AuthApi } from './auth-api';
+
+export default class Api {
+  public constructor() {
+    const configuredAxios = axios.create({
+      baseURL: process.env.API_URL,
+    });
+
+    this.axios = configuredAxios;
+    this.auth = new AuthApi(this.axios);
+  }
+
+  private axios: Axios;
+  public readonly auth: AuthApi;
+}
